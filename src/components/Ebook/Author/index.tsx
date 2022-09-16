ce3ccc4a7246cee5168277bfd8e86180732781d9
Adriano Almeida <adriano@z1.app>
2022-09-15 21:40:29 -0300

    refactor: new header

---
+++ src/components/Ebook/Author/index.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import {
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Avatar,
+} from "@chakra-ui/react"
+
+export default function Author({
+  ...rest
+}) {
+  return (
+    <Flex
+      as="section"
+      align="center"
+      mx="auto"      
+      justify="space-between"
+      direction={{ base: "column-reverse", md: "row" }}
+      minH="70vh"
+      w="100%"
+      px="6"
+      pb="10"
+      pt="6"
+      maxWidth={1480}
+    >
+      <Stack
+        spacing={4}
+        w={{ base: "80%", md: "60%", lg: "70%" }}
+        align={["center", "center", "flex-start", "flex-start"]}
+      >
+        <Heading
+          as="h1"
+          size="lg"
+          fontWeight="bold"
+          color="primary.800"
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Sobre o autor
+        </Heading>
+        <Heading
+          as="h2"
+          size="md"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Olá! Caso você tenha caído aqui de paraquedas, deixa eu me apresentar primeiro. Meu nome é Adriano Almeida, sou Software Enginner e nas horas vagas gosto de criar conteúdo sobre tecnologia e programação. Tenho mais de dez anos de mercado e comecei como programador full- stack.
+        </Heading>
+
+        <Heading
+          as="h2"
+          size="md"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Se você tem intenção de facilitar o seu processo de contratação na área de programação, então esse ebook vai te ensinar algumas facilidades que eu aprendi com muita experiência e mão na massa.
+          
+        </Heading>
+
+      </Stack>
+      <Box w={{ base: "80%", sm: "80%", md: "35%", lg: "30%" }} borderRadius="50%" mb={{ base: 12, md: 0 }}>
+        <Avatar src="https://www.github.com/adrianoluisalmeida.png" size="full"  borderColor="gray.800" borderWidth="15px" />
+      </Box>
+    </Flex>
+  )
+}
+
+
--- src/pages/ebook/index.tsx
+++ src/pages/ebook/index.tsx
@@ -1,20 +1,19 @@
 import { Avatar, Box, Button, Center, Container, Flex, Heading, Link, SimpleGrid, Stack, Text, WrapItem } from "@chakra-ui/react"
 import { NextPage } from "next"
 import { Header } from "../../components/Header"
-import EbookComponent from "../../components/Ebook"
+import EbookComponent from "../../components/Ebook/Header"
 import Footer from "../../components/Footer"
+import Author from "../../components/Ebook/Author"
 
 const Ebook: NextPage = () => {
   return (
     <Container maxW="1024px">
       <Header />
-
       <EbookComponent
         subtitle="Conheça as habilidades indispensáveis para acelerar o seu processo de contratação na oportunidade que sempre sonhou! "
-        title="O caminho mais fácil para conseguir a primeira oportunidade em programação!"
+        title="O Caminho Mais Fácil Para Você Se Tornar DEV!"
         key="1"
         image="https://s3publicimage.s3.amazonaws.com/ebook-mockup.png" />
-
       <Stack padding="10">
         <Heading
           as="h3"
@@ -38,63 +37,7 @@ const Ebook: NextPage = () => {
     
       </Stack>
 
-      <Flex
-      as="section"
-      align="center"
-      mx="auto"      
-      justify="space-between"
-      direction={{ base: "column-reverse", md: "row" }}
-      minH="70vh"
-      w="100%"
-      px="6"
-      pb="10"
-      pt="6"
-      maxWidth={1480}
-    >
-      <Stack
-        spacing={4}
-        w={{ base: "80%", md: "60%", lg: "70%" }}
-        align={["center", "center", "flex-start", "flex-start"]}
-      >
-        <Heading
-          as="h1"
-          size="lg"
-          fontWeight="bold"
-          color="primary.800"
-          textAlign={["center", "center", "left", "left"]}
-        >
-          Sobre o autor
-        </Heading>
-        <Heading
-          as="h2"
-          size="md"
-          color="primary.800"
-          opacity="0.8"
-          fontWeight="normal"
-          lineHeight={1.5}
-          textAlign={["center", "center", "left", "left"]}
-        >
-          Olá! Caso você tenha caído aqui de paraquedas, deixa eu me apresentar primeiro. Meu nome é Adriano Almeida, sou Software Enginner e nas horas vagas gosto de criar conteúdo sobre tecnologia e programação. Tenho mais de dez anos de mercado e comecei como programador full- stack.
-        </Heading>
-
-        <Heading
-          as="h2"
-          size="md"
-          color="primary.800"
-          opacity="0.8"
-          fontWeight="normal"
-          lineHeight={1.5}
-          textAlign={["center", "center", "left", "left"]}
-        >
-          Se você tem intenção de facilitar o seu processo de contratação na área de programação, então esse ebook vai te ensinar algumas facilidades que eu aprendi com muita experiência e mão na massa.
-          
-        </Heading>
-
-      </Stack>
-      <Box w={{ base: "80%", sm: "80%", md: "35%", lg: "30%" }} borderRadius="50%" mb={{ base: 12, md: 0 }}>
-        <Avatar src="https://www.github.com/adrianoluisalmeida.png" size="full"  borderColor="gray.800" borderWidth="15px" />
-      </Box>
-    </Flex>
+      <Author />
 
       <Stack padding="10">
         <Heading
