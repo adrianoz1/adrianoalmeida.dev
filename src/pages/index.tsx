16d7e8771a61c541352b4096b8038f53a1831632
adrianoz1 <canalpipeline@gmail.com>
2026-01-29 11:20:51 -0300

    upgrade packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Header } from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Footer from '../components/Footer'
-import Newsletter from '../components/Newsletter'
 
 const Home: NextPage = () => {
   return (
